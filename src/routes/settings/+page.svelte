654da8cfa61088dba60c8020efda8f91cfa790dc
Vasco Costa <vascomacosta@gmail.com>
2023-08-13 17:30:55 +0100

    Add /settings route

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let result: string = '';
+	let apiKey: string = '';
+
+	onMount(() => {
+		apiKey = localStorage.getItem('apiKey')?.toString() || '';
+	});
+
+	function saveSettings() {
+		try {
+			localStorage.setItem('apiKey', apiKey);
+		} catch (error) {
+			result = 'Could not save settings.';
+		}
+	}
+</script>
+
+<h1>Settings</h1>
+<div>{result}</div>
+<form id="eventForm" on:submit|preventDefault={saveSettings}>
+	<label for="apiKey">API key:</label>
+	<input type="password" id="apiKey" name="apiKey" bind:value={apiKey} />
+
+	<input type="submit" value="Save" />
+</form>

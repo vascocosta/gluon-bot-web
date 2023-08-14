ae4c60ad959decb71d9e2e948a39b19157bf7fe7
Vasco Costa <vascomacosta@gmail.com>
2023-08-14 17:33:56 +0100

    Add theme stub to settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 
 	let result: string = '';
+	let theme: string = '';
 	let apiKey: string = '';
 
 	onMount(() => {
@@ -22,8 +23,26 @@
 <h1>Settings</h1>
 <div>{result}</div>
 <form id="eventForm" on:submit|preventDefault={saveSettings}>
+	<label for="theme">Theme: (coming soon...)</label>
+	<select id="theme" name="theme" bind:value={theme}>
+		<option value="light" selected>light</option>
+		<option value="dark">dark</option>
+	</select>
+
 	<label for="apiKey">API key:</label>
 	<input type="password" id="apiKey" name="apiKey" bind:value={apiKey} />
 
 	<input type="submit" value="Save" />
 </form>
+
+<style>
+	form {
+		display: grid;
+		gap: 10px;
+		width: 25%;
+	}
+
+	label {
+		display: block;
+	}
+</style>

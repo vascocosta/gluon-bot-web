17cfc5f183496deb1b5de02f37ede11ab36a67d1
Vasco Costa <vascomacosta@gmail.com>
2023-08-15 22:46:00 +0100

    Add search to events

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -5,8 +5,16 @@
 
 	const URL: string = 'https://vettel.gluonspace.com/api/events';
 
+	let apiKey: string = '';
+	let category: string = '';
+	let channel: string = '';
+	let datetime: string = '';
+	let description: string = '';
 	let events: BotEvent[] = [];
+	let name: string = '';
+	let notify: boolean = false;
 	let result: string = '';
+	let tags: string = '';
 
 	onMount(async () => {
 		try {
@@ -21,9 +29,55 @@
 			result = `Could not parse events. (Error: ${error})`;
 		}
 	});
+
+	async function search(): Promise<void> {
+		try {
+			const response = await fetch(`${URL}?category=${category}` +
+												`&name=${name}` +
+												`&description=${description}`+
+												`&datetime=${datetime}` +
+												`&channel=${channel}` +
+												`&tags=${tags}`);
+
+			if (response.ok) {
+				events = [];
+				events = await response.json();
+			} else {
+				result = `Could not fetch events. (${response.status} ${statusText(response.status)})`;
+			}
+
+		} catch (error) {
+			result = `Could not parse events. (Error: ${error})`;
+		}
+	}
 </script>
 
 <div>{result}</div>
+<form id="eventForm" on:submit|preventDefault={search}>
+	<label for="category">Category:</label>
+	<input type="text" id="category" name="category" bind:value={category} />
+
+	<label for="name">Name:</label>
+	<input type="text" id="name" name="name" bind:value={name} />
+
+	<label for="description">Description:</label>
+	<input type="text" id="description" name="description" bind:value={description} />
+
+	<label for="datetime">Date/Time:</label>
+	<input type="text" id="datetime" name="datetime" bind:value={datetime} />
+
+	<label for="channel">Channel:</label>
+	<select id="channel" name="channel" bind:value={channel}>
+		<option value="#formula1" selected>#formula1</option>
+		<option value="#geeks">#geeks</option>
+		<option value="#nerds">#nerds</option>
+	</select>
+
+	<label for="tags">Tags:</label>
+	<input type="text" id="tags" name="tags" bind:value={tags} />
+
+	<input type="submit" value="Search" />
+</form>
 <table>
 	<thead>
 		<tr>

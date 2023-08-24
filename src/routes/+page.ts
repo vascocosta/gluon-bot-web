ea13c96abfba8f97f6108da76979a7bb8957c858
Vasco Costa <vascomacosta@gmail.com>
2023-08-24 14:01:08 +0100

    Replace fetch functions with a single generic one

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,40 +1,33 @@
 import { API_URL } from '../constants/constants';
+import { statusText } from '$lib/utils';
 import type { BotEvent, F1Bet, F1BetScore } from '../types/types';
 
 export function load({ params }) {
     return {
-        bets: fetchBets(),
-        events: fetchEvents(),
-        scores: fetchBetScores(),
+        bets: fetchData<F1Bet>('f1bets'),
+        events: fetchData<BotEvent>('events'),
+        scores: fetchData<F1BetScore>('f1bets/scores'),
     };
 }
 
-async function fetchEvents(): Promise<BotEvent[]> {
-    const response = await fetch(`${API_URL}/events`);
+async function fetchData<T>(endpoint: string): Promise<T[]> {
+    try {
+        const response = await fetch(`${API_URL}/${endpoint}`);
 
-    if (!response.ok) {
-        throw new Error('Could not fetch data.');
+        if (!response.ok) {
+            console.log(`Data could not be fetched. (${response.status} ${statusText(response.status)}`);
+
+            return Promise.resolve([]);
+        }
+
+        return await response.json();
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(`Data could not be parsed. (${error.message})`);
+        } else {
+            console.log(`Data could not be parsed. (Unknown error)`);
+        }
+
+        return Promise.resolve([]);
     }
-
-    return await response.json();
-}
-
-async function fetchBets(): Promise<F1Bet[]> {
-    const response = await fetch(`${API_URL}/f1bets`);
-
-    if (!response.ok) {
-        throw new Error('Could not fetch data.');
-    }
-
-    return await response.json();
-}
-
-async function fetchBetScores(): Promise<F1BetScore[]> {
-    const response = await fetch(`${API_URL}/f1bets/scores`);
-
-    if (!response.ok) {
-        throw new Error('Could not fetch data.');
-    }
-
-    return await response.json();
 }
